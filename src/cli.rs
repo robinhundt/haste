dd8d8ed8ba62112d347e5dc5784c2f08213022c6
robinhundt <24554122+robinhundt@users.noreply.github.com>
2025-11-22 15:50:34 +0100

    Doc improvements

--- src/cli.rs
+++ src/cli.rs
@@ -2,8 +2,9 @@ use clap::Parser;
 use regex_lite::Regex;
 
 #[derive(Parser)]
+/// The haste benchmark runner.
 pub(crate) struct Cli {
-    /// Filter your benchmarks with the provided regex
+    /// Filter your benchmarks with the provided regex.
     pub(crate) filter: Option<Regex>,
     #[clap(long, hide(true))]
     bench: bool,
